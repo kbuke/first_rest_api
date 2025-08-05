42d81a4c4a4a94b68e4efeb9caf7faec219e1ade
Kaan Buke <kabuke13@gmail.com>
2025-08-05 14:35:00 +0200

    allows user to access store endpoint

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+stores = [
+    {
+        "name": "My store",
+        "items": [
+            {
+                "name": "Chair",
+                "price": 15.99
+            }
+        ]
+    }
+]
+
+@app.get("/store") # this is the endpoint
+def get_stores():
+    return{"stores": stores}
